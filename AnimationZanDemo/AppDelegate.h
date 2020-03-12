9d961325ee4f13ae85f727321c349ed36befb054
Xdf <zhanggaotong@xdf.cn>
2020-03-12 15:51:44 +0800

    Initial Commit

---
+++ AnimationZanDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnimationZanDemo
+//
+//  Created by Xdf on 2020/3/12.
+//  Copyright © 2020 Xdf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AnimationZanDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  AnimationZanDemo
+//
+//  Created by Xdf on 2020/3/12.
+//  Copyright © 2020 Xdf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
